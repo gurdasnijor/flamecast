e2f5778f3a35ee9b995c7cf7d62c4066052b73bc
Anirudh Kamath <github@kamath.io>
2026-03-22 15:09:13 -0700

    refactor: move flamecast signal handling into core (SMI-1664) (#26)
    * refactor: move flamecast signal handling into core
    
    * simplify startServer

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,28 +1,10 @@
 import { pathToFileURL } from "node:url";
 import { Flamecast } from "@acp/flamecast";
 
-export function createShutdownHandler(flamecast: Flamecast, server: { close: () => void }) {
-  return async function shutdown() {
-    console.log("\nShutting down...");
-    await flamecast.shutdown();
-    server.close();
-    process.exit(0);
-  };
-}
-
-export async function startServer() {
-  const flamecast = new Flamecast();
-  const server = await flamecast.listen(3001);
-  const shutdown = createShutdownHandler(flamecast, server);
-
-  process.on("SIGTERM", shutdown);
-  process.on("SIGINT", shutdown);
-
-  return { flamecast, server, shutdown };
-}
-
 export async function main() {
-  return startServer();
+  const flamecast = new Flamecast();
+  await flamecast.listen(3001);
+  return flamecast;
 }
 
 function isMainModule(): boolean {
